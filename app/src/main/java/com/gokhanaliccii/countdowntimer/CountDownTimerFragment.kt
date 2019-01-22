f23208abe24b53b4da0e9e2e3e4a34fe39b4e53e
alici,gokhan <gokhanaliccii@gmail.com>
2019-01-22 22:31:48 +0300

    Count down functionality added on viewmodel

--- app/src/main/java/com/gokhanaliccii/countdowntimer/CountDownTimerFragment.kt
+++ app/src/main/java/com/gokhanaliccii/countdowntimer/CountDownTimerFragment.kt
@@ -1,18 +1,53 @@
 package com.gokhanaliccii.countdowntimer
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.gokhanaliccii.countdowntimer.countdown.CountDownViewModel
+import com.gokhanaliccii.countdowntimer.countdown.UIState
+import com.gokhanaliccii.countdowntimer.databinding.FragmentCountdownBinding
 
 class CountDownTimerFragment : Fragment() {
+
+    lateinit var viewModel: CountDownViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this)[CountDownViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_countdown, container, false)
+        val layoutBinding = FragmentCountdownBinding.inflate(inflater, container, false)
+        layoutBinding.setLifecycleOwner(this)
+        layoutBinding.viewModel = viewModel
+
+        return layoutBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewModel.start()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.uiState.observe(this, Observer {
+            when (it) {
+                is UIState.Completed -> {
+                
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/gokhanaliccii/countdowntimer/countdown/CountDownViewModel.kt
+++ app/src/main/java/com/gokhanaliccii/countdowntimer/countdown/CountDownViewModel.kt
@@ -1,4 +1,79 @@
 package com.gokhanaliccii.countdowntimer.countdown
 
-class CountDownViewModel {
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.gokhanaliccii.countdowntimer.countdown.UIState.*
+import com.gokhanaliccii.countdowntimer.countdown.checker.ExtensibilityBySecondChecker
+import com.gokhanaliccii.countdowntimer.countdown.formatter.RemainSecondFormatter
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicLong
+
+class CountDownViewModel : ViewModel() {
+
+    private companion object {
+        const val TWO_MINUTE = 120000L
+        const val TEN_SECOND = 10000L
+        const val HUNDRED_MILLI_SECOND = 100L
+        const val INITIAL_DISPLAYED_TEXT = "2 : 0 : 0"
+    }
+
+    val uiState = MutableLiveData<UIState>()
+    val displayedText = MutableLiveData<String>()
+
+    private lateinit var disposable: Disposable
+    private val remainSecondFormatter = RemainSecondFormatter()
+    private val timeExtensibility = ExtensibilityBySecondChecker(
+        TEN_SECOND, TWO_MINUTE
+    )
+
+    private val remainMillis = AtomicLong(TWO_MINUTE)
+
+
+    init {
+        uiState.postValue(Initial)
+        displayedText.postValue(INITIAL_DISPLAYED_TEXT)
+    }
+
+    fun start() {
+        uiState.postValue(Counting)
+
+        disposable = Observable.interval(HUNDRED_MILLI_SECOND, TimeUnit.MILLISECONDS)
+            .takeUntil {
+                remainMillis.get() <= 0L
+            }.map {
+                (remainMillis.get() - it)
+            }.doOnNext {
+                remainMillis.set(it!!)
+            }.map(remainSecondFormatter::format)
+            .doOnComplete {
+                uiState.postValue(Completed)
+            }
+            .subscribe {
+                displayedText.postValue(it)
+            }
+    }
+
+    fun incrementRemain() {
+        val remain = remainMillis.get()
+
+        if (timeExtensibility.isTimeExtensible(remain)) {
+            remainMillis.set(remain + TimeUnit.SECONDS.toMillis(10))
+        } else if (timeExtensibility.isTimeExtensible(remain)) {
+            reset()
+        }
+    }
+
+    private fun reset() {
+        remainMillis.set(TWO_MINUTE)
+        displayedText.postValue(INITIAL_DISPLAYED_TEXT)
+        uiState.postValue(Counting)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        disposable.dispose()
+    }
 }
