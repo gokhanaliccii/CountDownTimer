79d552edf43152a575e1a823cabcc8ac6d72505b
alici,gokhan <gokhanaliccii@gmail.com>
2019-01-22 15:56:37 +0300

    CountDownFragment created and with this fragment fragment_countdown.xml inflated

--- app/src/main/java/com/gokhanaliccii/countdowntimer/MainActivity.kt
+++ app/src/main/java/com/gokhanaliccii/countdowntimer/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        savedInstanceState.let {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.content_frame, CountDownTimerFragment())
+                .commit()
+        }
+
     }
 }
